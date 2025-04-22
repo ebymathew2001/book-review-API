52f6a9db1967b3622302d13af0959e08304e3ebd
Eby Mathew <ebykannamkulathu@gmail.com>
2025-04-22 22:34:27 +0530

    initial commit

---
+++ src/test/java/com/example/demo/BookReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
